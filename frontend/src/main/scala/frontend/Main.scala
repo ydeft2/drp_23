da53730646dc4d4e35b681ab76f1c6f260fa2ce2
Tabeer Asif <tabeerasif5@gmail.com>
2025-06-03 18:48:55 +0100

    Main renders booking dashboard only for viewing

--- frontend/src/main/scala/frontend/Main.scala
+++ frontend/src/main/scala/frontend/Main.scala
@@ -6,33 +6,36 @@ import scalajs.concurrent.JSExecutionContext.Implicits.queue
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val tokenOpt = Option(dom.window.localStorage.getItem("accessToken"))
-    tokenOpt match {
-      case Some(token) =>
-        println("Token found: " + token)
-        verifyToken(token).foreach { valid =>
-          println(s"Token valid? $valid")
-          if (valid) {
-            isPatient().map { isPatient =>
-              if (isPatient) {
-                HomePage.render()
-              } else {
-                AdminPage.render()
-              }
-            }.recover {
-              case e: Throwable => dom.window.alert(s"An error occurred: ${e.getMessage}")
-            }
-          } else {
-            println("Invalid token, removing and rendering login")
-            dom.window.localStorage.removeItem("accessToken")
-            dom.window.localStorage.removeItem("userId")
-            LoginPage.render()
-          }
-        }
-      case None =>
-        println("No token found, rendering login")
-        LoginPage.render()
-    }
+    BookingDashboard.render()
+    // val tokenOpt = Option(dom.window.localStorage.getItem("accessToken"))
+    // tokenOpt match {
+    //   case Some(token) =>
+    //     println("Token found: " + token)
+    //     verifyToken(token).foreach { valid =>
+    //       println(s"Token valid? $valid")
+    //       if (valid) {
+    //         isPatient().map { isPatient =>
+    //           if (isPatient) {
+    //             HomePage.render()
+                
+    //           } else {
+    //             AdminPage.render()
+    //           }
+    //         }.recover {
+    //           case e: Throwable => dom.window.alert(s"An error occurred: ${e.getMessage}")
+    //         }
+    //       } else {
+    //         println("Invalid token, removing and rendering login")
+    //         dom.window.localStorage.removeItem("accessToken")
+    //         dom.window.localStorage.removeItem("userId")
+    //         LoginPage.render()
+            
+    //       }
+    //     }
+    //   case None =>
+    //     println("No token found, rendering login")
+    //     LoginPage.render()
+    // }
   }
 }
 
