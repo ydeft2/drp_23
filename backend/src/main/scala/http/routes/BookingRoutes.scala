4c1a7fa4c8383f4bfe144d32691fcf2165e27a94
Jonny <j.sinteix@gmail.com>
2025-06-05 21:06:47 +0100

    implements skeleton booking route endpoints

---
+++ backend/src/main/scala/http/routes/BookingRoutes.scala
@@ -0,0 +1,48 @@
+package http.routes
+
+import cats.effect.IO
+import org.http4s.HttpRoutes
+import org.http4s.dsl.Http4sDsl
+import org.http4s.server.*
+
+
+class BookingRoutes private extends Http4sDsl[IO] {
+
+  // TODO: i guess theres both a patient and clinic view of this
+  // maybe they will have different filters
+  private val listAllBookingsRoute: HttpRoutes[IO] = HttpRoutes.of[IO] {
+    case GET -> Root =>
+      Ok("getting ma bookings")
+  }
+
+  private val getBookingByIdRoute: HttpRoutes[IO] = HttpRoutes.of[IO] {
+    case GET -> Root =>
+      Ok("getting a specific booking")
+  }
+
+  private val cancelBookingsRoute: HttpRoutes[IO] = HttpRoutes.of[IO] {
+    case GET -> Root / "cancel" / UUIDVar(slotId) =>
+      Ok("cancelling mai bookings")
+  }
+
+  // TODO: so hopefully a clinic can update, say, with an important message
+  // ideally this could be in the form of a sort of chat
+  // so could be the case that maybe a patient could potentially update a booking, but for the chat feature only
+  private val updateBookingsRoute: HttpRoutes[IO] = HttpRoutes.of[IO] {
+    case GET -> Root / "update" =>
+      Ok("updating my bookings")
+  }
+
+  val routes = Router(
+    "/bookings" -> (
+      listAllBookingsRoute
+      )
+  )
+
+}
+
+object BookingRoutes {
+
+  def apply(): BookingRoutes = new BookingRoutes
+
+}
