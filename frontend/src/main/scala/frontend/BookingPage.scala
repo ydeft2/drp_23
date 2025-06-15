ff1aad951eb96beb038ecdaa4c3bb0d85bbb7bd9
Jonny <j.sinteix@gmail.com>
2025-06-15 20:50:59 +0100

    Timetable now 8am-6pm including midday

--- frontend/src/main/scala/frontend/BookingPage.scala
+++ frontend/src/main/scala/frontend/BookingPage.scala
@@ -13,7 +13,7 @@ import scala.concurrent.Future
 
 object BookingPage {
   // --- Common state for both views ---
-  private val timeStrings = Seq("09:00","10:00","11:00","13:00","14:00","15:00","16:00")
+  private val timeStrings = Seq("08:00","09:00","10:00","11:00","12:00", "13:00","14:00","15:00","16:00", "17:00")
   private val zoneId      = ZoneOffset.UTC
   private val displayFmt  = DateTimeFormatter.ofPattern("dd MMM yyyy")
 
